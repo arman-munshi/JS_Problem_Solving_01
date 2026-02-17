f26302e537d7d89ca9797762991ba6644320c352
arman115data <mdarmaanmunshi@gmail.com>
2026-02-17 17:47:38 +0600

    solve how to remove a duplicate item from an array

--- duplicate.js
+++ duplicate.js
@@ -2,4 +2,21 @@
  * array has some duplicate elements
  */
 
-const biryanikhor =["abul","kabul","cabul","abul","babul","dabul"]
+const biryanikhor = ["abul", "kabul", "cabul", "abul", "babul", "dabul"]
+const number = [1, 5, 61, 5, 87, 7, 5, 81, 61];
+
+function noDuplicate(array) {
+    // console.log(array);
+    const unique=[]
+    for (const item of array) {
+        if (unique.includes(item) === false) {
+            unique.push(item);
+        }
+
+        
+    }
+    return unique;
+}
+
+const uniqueArray = noDuplicate(number);
+console.log(uniqueArray)
